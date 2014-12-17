393a7e15b7aca942b9e2c88824cf26a80325d453
Karla Saur <ksaur@umd.edu>
2014-12-17 17:18:30 -0500

    adding missed flag to shared makefile

--- examples/shared.mk
+++ examples/shared.mk
@@ -1,5 +1,5 @@
 EKINC  = -I../../bin/
-CSTR_FLAGS = -ggdb3 -Wall -ldl -lrt -I../../contrib/uthash/  
+CSTR_FLAGS = -ggdb3 -Wall -ldl -lrt -I../../contrib/uthash/ -include cstrider_api.h 
 EKCC = ../../bin/ktcc
 EKGEN = ../../bin/xfgen
 EKCC = ../../bin/ktcc
